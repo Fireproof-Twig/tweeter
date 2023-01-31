9fd894d0ba79ad7ddf96c711f295effb2b77ee84
Chris Baltis <cbaltis@gmail.com>
2023-01-31 21:24:50 +0000

    created a js file and using jquery got the counter working in the browser

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,34 @@
+
+
+
+$(document).ready(function() {
+  // const textArea = document.getElementById("tweet-text")
+  // const printMessage = () => {
+  //   console.log("experimented with text area")
+  // }
+
+  // document.addEventListener("click", () => {
+  //   alert("You clicked somewhere on tweeter");
+  // });
+  console.log("bonjour")
+  let maximumLength = 140;
+  textArea = $('#tweet-text');
+  textArea.on('input', function() {
+    let remainingCharacter = maximumLength - $(this).val().length;
+    const counter = $(this).parent().find('.counter');
+    if (remainingCharacter >= 0) {
+      // $('.counter').removeClass("negative")
+      counter.val(remainingCharacter);
+    }
+    else {
+      $('.counter').addClass("negative")
+      counter.val(remainingCharacter);
+    }
+ 
+  
+  })
+
+  // textArea.addEventListener('click', () => {
+  //   alert("hello there")
+  // });
+});
